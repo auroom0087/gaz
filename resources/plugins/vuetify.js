de21b1b698be5b9bc5fb2ce0e0a407de5a8214e6
Khramov Sergey <56201987+hramov@users.noreply.github.com>
2020-06-15 18:26:02 +0300

    mdi icons

--- resources/plugins/vuetify.js
+++ resources/plugins/vuetify.js
@@ -1,12 +1,14 @@
+// src/plugins/vuetify.js
+
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
-const opts = {}
-
 export default new Vuetify({
   icons: {
-    iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    iconfont: 'mdi', // default - only for display purposes
+    iconfont: 'mdiSvg',
   },
 })
