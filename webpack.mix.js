3732fc0a66524b24cac878aa87c7b805b1c80439
Asper <varfol0077@gmail.com>
2020-06-15 16:38:27 +0300

    add App.vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-const VuetifyLoaderPlugin = require ('vuetify-loader/lib/plugin')
+const VuetifyLoaderPlugin = require ('vuetify-loader/lib/plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 var webpackConfig = {
@@ -12,7 +12,7 @@ var webpackConfig = {
     // other webpack config ...
 }
 
-mix.webpackConfig( webpackconfig );
+mix.webpackConfig(webpackConfig);
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
