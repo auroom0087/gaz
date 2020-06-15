9d742374378c5fa05fe5c95db98805a580d0ee38
Khramov Sergey <56201987+hramov@users.noreply.github.com>
2020-06-15 17:59:07 +0300

    123123123
    213213123123

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,13 @@
 <template>
     <div>
-        <Navbar />
         <router-view></router-view>
-        <Footer />
     </div>
 </template>
 
 <script>
-    import Navbar from './components/Navbar'
-    import Footer from './components/Footer'
     export default {
         components: {
-            Navbar,
-            Footer
+
         }
     }
 </script>
