8907d625d00af22881d7c2948bbc2f3ebc9d3147
Khramov Sergey <56201987+hramov@users.noreply.github.com>
2020-06-15 18:08:02 +0300

    123

--- public/js/app.js
+++ public/js/app.js
@@ -95117,8 +95117,8 @@ var opts = {};
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Astra\Desktop\web\vue_projects\gaz\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Astra\Desktop\web\vue_projects\gaz\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\hack\gaz\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\hack\gaz\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
